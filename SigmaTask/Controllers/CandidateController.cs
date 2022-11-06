a1a5baef6fd5cc285bde4a244bcbaabbf01c5d38
islam <islamabdallah88@gmail.com>
2022-11-06 21:44:45 +0200

    Add WebApi (EndPoint) used by UI Return Candidate List Save Candidate (Add / Update)

---
+++ SigmaTask/Controllers/CandidateController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SigmaTask.Models;
+using SigmaTask.Repositories;
+using System.Threading.Tasks;
+
+namespace SigmaTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateRepo CandidateRepo;
+        public CandidateController(ICandidateRepo candidateRepo)
+        {
+            CandidateRepo = candidateRepo;
+        }
+
+        [HttpGet("CandidateList")]
+        public IActionResult GetCandidateList()
+        {
+            return Ok(CandidateRepo.CandidateList());
+        }
+
+        [HttpPost("SaveCandidate")]
+        public IActionResult SaveCandidate(Candidate candidate)
+        {
+            return Ok(CandidateRepo.SaveCandidate(candidate));
+        }
+    }
+}
