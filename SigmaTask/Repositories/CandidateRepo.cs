4f5c1bb54ae88ead8d51bbbc1b25c15b1d050459
islam <islamabdallah88@gmail.com>
2022-11-06 11:40:51 +0200

    Candidate Model & Repositories Structure

---
+++ SigmaTask/Repositories/CandidateRepo.cs
@@ -0,0 +1,23 @@
+﻿using SigmaTask.Models;
+using System.Collections.Generic;
+
+namespace SigmaTask.Repositories
+{
+    public class CandidateRepo : ICandidateRepo
+    {
+        public List<Candidate> CandidateList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Candidate GetCandidate(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Candidate SaveCandidate(Candidate candidate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ SigmaTask/Repositories/ICandidateRepo.cs
@@ -0,0 +1,12 @@
+﻿using SigmaTask.Models;
+using System.Collections.Generic;
+
+namespace SigmaTask.Repositories
+{
+    public interface ICandidateRepo
+    {
+        List<Candidate> CandidateList();
+        Candidate GetCandidate(string email);
+        Candidate SaveCandidate(Candidate candidate);
+    }
+}
