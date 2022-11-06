f90df7189bd6ad91f4d219afc7ec0c72ccdaacb4
islam <islamabdallah88@gmail.com>
2022-11-06 10:56:30 +0200

    Add project files.

---
+++ SigmaTask/Models/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SigmaTask.Models
+{
+    public class Candidate
+    {
+        [Key]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string TimeInterval { get; set; }
+        public string GitHub { get; set; }
+        public string Linkedin { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
